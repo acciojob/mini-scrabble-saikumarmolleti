de4e3274913753323cab77af99e18e8064fc7716
Saikumar Molleti <80804527+saikumarmolleti@users.noreply.github.com>
2022-12-28 18:35:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+let input = document.getElementsByTagName('input')[0];
+console.log(input);
+let count = 0;
+input.addEventListener('keyup',realtime);
+function realtime(){
+   let str = input.value;
+   document.getElementById('letterCount').innerText = str.length;
+}
